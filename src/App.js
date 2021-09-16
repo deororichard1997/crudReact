f124eebbf49dcbdeb17be0a2befb838b0140e65e
Richard de oro <alcazarrichard19976@gmail.com>
2021-09-16 00:35:49 -0500

    feat: add task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
+import shortid from "shortid";
+//import { isEmpty } from 'lodash';
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (task === "") {
+      console.log("Task empty");
+      return;
+    }
+
+    const newTask = {
+      id: shortid.generate(),
+      name: task,
+    };
+
+    setTasks([...tasks, newTask]);
+    console.log("ok");
+    console.log(tasks);
+    setTask("");
+  };
+
   return (
     <div className="container mt-5">
       <h1>Tareas</h1>
-      <hr/>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            {tasks.map((task) => (
+              <li className="list-group-item" key={task.id}>
+                <span className="lead">{task.name}</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">
+                  Eliminar
+                </button>
+                <button className="btn btn-warning btn-sm float-right">
+                  Editar
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingesar tarea..."
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
+            />
+            <button className="btn btn-dark btn-block" typr="submit">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
